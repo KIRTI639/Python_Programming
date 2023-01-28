ccdd40788c64eadb0b71e9fd244d276d8e097ea6
Kirti <kirtirajput63969@gmail.com>
2023-01-29 01:11:42 +0530

    Simple python file

---
+++ p1.py
@@ -0,0 +1,9 @@
+#  Write a Python program to print the following string in a specific format (see the output). 
+# Output :
+# Twinkle, twinkle, little star,
+#     How I wonder what you are!
+#         Up above the world so high,
+#         Like a diamond in the sky.
+# Twinkle, twinkle, little star,
+#     How I wonder what you are
+print("Twinkle, twinkle, little star,\n\t How I wonder what you are!\n\t\t Up above the world so high,\n\t\t Like a diamond in the sky.\n Twinkle, twinkle, little star,\n\tHow I wonder what you are")
