11e82cfb293c1710669a6047b5e24e26ac0d918f
Kirti <kirtirajput63969@gmail.com>
2023-02-04 21:28:43 +0530

    Check even and odd numbers

---
+++ even_odd.py
@@ -0,0 +1,12 @@
+# An Integer is Input through keyboard. Write a program to find whether it is odd or even number.
+
+
+# n = int(input("Enter the number :"))
+# print("Even" if n%2==0 else "Odd")
+
+
+num = int(input("Enter the number :"))
+if num%2==0:
+    print("Given number is a odd number")
+else:
+    print("Given number is a odd number")    
