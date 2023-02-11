69cee11a00fd61d4b99e859727aa63b134cb0ed9
Kirti <kirtirajput63969@gmail.com>
2023-02-11 14:22:53 +0530

    Python program to validate the mobile number

---
+++ mobile_number_validation.py
@@ -0,0 +1,7 @@
+a = (input("Enter the Mobile Number..."))
+if (a.startswith("+91")) and (len(a)==13):
+    print("Valid Mobile number....")
+else:
+    print("Not a Valid number...") 
+input()       
+
