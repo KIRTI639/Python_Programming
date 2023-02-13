1dcf4c702cae45c0e7762a5804107d8d6ed10655
Kirti <kirtirajput63969@gmail.com>
2023-02-13 23:35:08 +0530

    reverse_even_odd number

---
+++ reverse_even_odd.py
@@ -0,0 +1,13 @@
+# WAP to print all even and odd numbers between 100 and 1
+# a = 100
+# while(a>=1):
+#     if a%2==0:
+#         print(f"{a} is even.")
+#     else:
+#         print(f"{a} is odd.")
+#     a-=1       
+for a in range(100,0,-1):
+    if a%2==0:
+        print(f"{a} is even")
+    else:
+        print(f"{a} is a odd.")    
