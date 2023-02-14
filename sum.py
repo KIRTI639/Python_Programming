d770660b8bde2960b6f7d3834b7bfb29e11a9305
Kirti <kirtirajput63969@gmail.com>
2023-02-14 23:08:42 +0530

    Sum of numbers

---
+++ sum.py
@@ -0,0 +1,12 @@
+# WAP to print sum of numbers from 1 and 100;
+# a = 1
+# sum = 0
+# while(a<=100):
+#     sum = sum+a
+#     a+=1
+# print(f"Sum of numbers from 1 and 100 is {sum}.")
+sum = 0
+for a in range(1,101):
+    sum = sum+a
+print(f"Sum of numbers from 1 and 100 is {sum}.")    
+
